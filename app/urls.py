b24d6972be21ae5418e9bfaacbe3d1d5027a30aa
Jaotarzan <joaovssouza59@gmail.com>
2025-05-25 15:10:40 -0300

    feat: inclusão da app de upload e associação de imagens

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
     SpectacularSwaggerView,
 )
 from rest_framework.routers import DefaultRouter
-
+from uploader.router import router as uploader_router
 
 from core.views import CategoriaViewSet, EditoraViewSet, UserViewSet, AutorViewSet, LivroViewSet
 
@@ -33,4 +35,9 @@ urlpatterns = [
     ),
     # API
     path('api/', include(router.urls)),
+    path("api/media/", include(uploader_router.urls)),
+
 ]
+
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
+
--- core/models/livro.py
+++ core/models/livro.py
@@ -2,8 +2,17 @@ from django.db import models
 from .categoria import Categoria
 from .editora import Editora
 from .autor import Autor
+from uploader.models import Image
 
 class Livro(models.Model):
+    capa = models.ForeignKey(
+        Image,
+        related_name="+",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        default=None,
+    )
     titulo = models.CharField(max_length=255)
     isbn = models.CharField(max_length=32, null=True, blank=True)
     quantidade = models.IntegerField(default=0,  null=True, blank=True)
